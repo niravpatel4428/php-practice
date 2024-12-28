1ffd456c59328a784d227017d6065d1f7d3ae286
Nirav <nirav.jivani6@gmail.com>
2024-12-28 17:21:22 +0530

    array splice php

---
+++ practice-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<pre>
+
+/* array splice means remove */
+<?php
+    $cars = array("volvo","BMW","Toyota");
+    array_splice($cars, 1, 2);
+    var_dump($cars);
+?>
+
+/* remove array from associative array */
+
+<?php
+    
+?>
+
+
+</pre>
+</body>
+</html>

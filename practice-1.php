1f2b8d1575b20505cabdf4321e278eb9b3dd3539
nirav.jivani <nirav.jivani6@gmail.com>
2024-12-30 14:21:16 +0530

    first commit

--- practice-1.php
+++ practice-1.php
@@ -11,9 +11,25 @@
 ?>
 
 /* remove array from associative array */
-
 <?php
-    
+    $car1 = array("volvo","BMW","Toyota");
+    unset($car1[1]);
+    var_dump($car1);
+?>
+
+/* Remove Multiple Array Items */
+<?php
+    $car2 = array("volvo","BMW","Toyota");
+    unset($car2[0],$car2[1]);
+    var_dump($car2);
+?>
+
+/* Remove the Last Item */
+<?php
+    $cars3 = array("volvo","BMW","toyota");
+    array_pop($cars3); //Last item remove
+    array_shift($cars3); //first Item remove
+    var_dump($cars3);
 ?>
 
 
